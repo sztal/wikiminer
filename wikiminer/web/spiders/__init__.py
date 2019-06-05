448f4cef1b05b63fbe344fcc67ab8016e9354eef
sztal <stalaga@protonmail.com>
2019-06-05 13:44:06 +0200

    Add simple allpages spider

--- wikiminer/web/spiders/__init__.py
+++ wikiminer/web/spiders/__init__.py
@@ -2,6 +2,7 @@
 import json
 from scrapy import Spider
 from furl import furl
+from cerberus import Validator
 
 
 class ApiSpider(Spider):
@@ -13,6 +14,9 @@ class ApiSpider(Spider):
         Base url of the API. Defaults to: ``https://en.wikipedia.org/w/api.php``.
     """
     base_url = 'https://en.wikipedia.org/w/api.php'
+
+    _attributes_schema = {}
+
 
     def make_url(self, url=None, **kwds):
         """Make url.
@@ -50,3 +54,27 @@ class ApiSpider(Spider):
         """Parse JSON response."""
         data = json.loads(response.body_as_unicode())
         return data
+
+    def parse_attributes(self, return_null=True, **kwds):
+        """Parse, set and return attributes dictionary.
+
+        Parameters
+        ----------
+        return_null : bool
+            Should null value be returned.
+        **kwds :
+            Passed to :py:class:`cerberus.Validator` initialization.
+        """
+        v = Validator(self._attributes_schema, **kwds)
+        dct = {}
+        for k in v.schema:
+            if hasattr(self, k):
+                dct[k] = getattr(self, k)
+        dct = v.validated(dct)
+        if not dct:
+            raise ValueError(f"invalid attributes: {v.errors}")
+        for k, v in dct.items():
+            setattr(self, k, v)
+        if not return_null:
+            dct = { k: v for k, v in dct.items() if v is not None }
+        return dct
