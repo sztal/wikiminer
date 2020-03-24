056400391bfa918e23926b58da94c715c88202d6
sztal <stalaga@protonmail.com>
2020-03-24 13:30:10 +0100

    change api_userpages_cirrus (perhaps needs to be fixed)

--- wikiminer/web/spiders/api_userpages_cirrus.py
+++ wikiminer/web/spiders/api_userpages_cirrus.py
@@ -1,11 +1,13 @@
 # pylint: disable=no-member,protected-access
 from more_itertools import chunked
+import jmespath as jmp
 from scrapy import Request
-from .api_pages_cirrus import ApiPagesCirrus
+from dzeta.schema import Schema, fields
+from . import ApiSpider
 from ... import _
 
 
-class ApiUserpagesCirrus(ApiPagesCirrus):
+class ApiUserpagesCirrus(ApiSpider):
     """API spider for updating userpages cirrus data.
 
     _Attributes_ section described available user-provided arguments.
@@ -26,40 +28,91 @@ class ApiUserpagesCirrus(ApiPagesCirrus):
     """
     name = 'api_userpages_cirrus'
 
-    def make_start_requests(self, **kwds):
-        query = {}
-        if self.args.model is not None:
-            query['_cls'] = self.args.model
-        project = { '$project': { 'user_name': 1 } }
-        lookup = { '$lookup': {
-            'from': _.UserPage._.get_collection().name,
-            'let': { 'user_name': '$user_name' },
-            'pipeline': [
-                { '$match': {
-                    '_cls': _.UserPage._class_name,
-                    '$expr': {
-                        '$eq': [ '$user_name', '$$user_name' ]
-                    }
-                } },
-                { '$project': { '_id': 1 } }
-            ],
-            'as': 'pages'
-        } }
-        if self.args.missing_only:
-            lookup['$lookup']['pipeline'][0]['$match'].update(
-                source_text={ '$exists': False, '$in': [ None, [] ] }
-            )
-        add_fields = { '$addFields': { 'pages': '$pages._id' } }
-        unwind = { '$unwind': '$pages' }
+    class Args(Schema):
+        ns = fields.Int(missing=2, strict=False, validate=[
+            lambda x: x in (2, 3)
+        ])
+        limit = fields.Int(missing=50, strict=False, validate=[
+            lambda x: 0 < x <= 50
+        ])
+        missing_only = \
+            fields.Bool(missing=False, truthy=('yes', 'true'), falsy=('no', 'false'))
+        apfilterredir = fields.String(missing='nonredirects', validate=[
+            lambda x: x in ('nonredirects', 'redirects', 'all')
+        ])
 
-        cursor = _.User.objects.aggregate(
-            { '$match': query }, project, lookup, add_fields, unwind,
-            allowDiskUse=True
+    def make_ap_request(self, apprefix, **kwds):
+        url = self.make_query(
+            list='allpages',
+            aplimit=self.args.limit,
+            apfilterredir=self.args.apfilterredir,
+            apprefix=apprefix,
+            apnamespace=self.args.ns,
+            **kwds
         )
-        for chunk in chunked(cursor, n=self.args.limit):
-            url = self.make_query(
-                prop='cirrusdoc',
-                pageids='|'.join(str(doc['pages']) for doc in chunk),
-                **kwds
+        return Request(url, callback=self.parse_ap, meta={
+            'user_name': apprefix
+        })
+
+    def make_cirrus_request(self, user_name, page_ids, **kwds):
+        url = self.make_query(
+            prop='cirrusdoc',
+            pageids='|'.join(str(pid) for pid in page_ids),
+            **kwds
+        )
+        return Request(url, callback=self.parse_cirrus, meta={
+            'user_name': user_name
+        })
+
+    def parse_ap(self, response):
+        data = super().parse(response)
+
+        apcontinue = jmp.search('continue.apcontinue', data)
+        user_name = response.meta['user_name']
+        if apcontinue:
+            request_ap = self.make_ap_request(
+                apprefix=user_name,
+                apcontinue=apcontinue
             )
-            yield Request(url)
+            yield request_ap
+
+        pages = jmp.search('query.allpages', data)
+        if pages:
+            page_ids = [ p['pageid'] for p in pages ]
+            request_cirrus = self.make_cirrus_request(user_name, page_ids)
+            yield request_cirrus
+
+    def parse_cirrus(self, response):
+        data = super().parse(response)
+        pages = list(jmp.search('query.pages', data).values())
+        for page in pages:
+            if 'missing' in page:
+                continue
+            try:
+                cirrus = page.pop('cirrusdoc')[0]
+            except (KeyError, IndexError):
+                continue
+            source = cirrus['source']
+            page.update(
+                user_name=response.meta['user_name'],
+                page_type=cirrus['type'],
+                source_text=source['source_text'],
+                template=source['template'],
+                timestamp_updated=source['timestamp'],
+                timestamp_created=source['create_timestamp']
+            )
+            yield page
+
+    def make_start_requests(self, **kwds):
+        cursor = _.User.objects.aggregate(
+            { '$project': {
+                '_id': 0,
+                'user_name': 1
+            } }
+        )
+        for doc in cursor:
+            request = self.make_ap_request(apprefix=doc['user_name'])
+            yield request
+
+    def start_requests(self):
+        yield from self.make_start_requests()
